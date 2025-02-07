4fa7a02113239dd7fbbf94dd751442190edacc58
Mohamed Medhat <mmedhat1910@gmail.com>
2025-02-07 22:49:57 +0200

    Unit testing

--- src/test/java/com/example/lab1/Lab12ApplicationTests.java
+++ src/test/java/com/example/lab1/Lab12ApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.lab1;
 
+import com.example.lab1.models.User;
+import com.example.lab1.services.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Lab12ApplicationTests {
 
+	@Autowired
+	private UserService userService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void createUser_withValidInput_shouldReturnSameNameAndEmail() {
+		// Arrange
+		User newUser = new User("Bob", "bob@java.com", 30);
+
+		// Act
+		User createdUser = userService.createUser(newUser);
+
+		// Assert
+		assertEquals(createdUser.getName(),newUser.getName(), "Name is not the same");
+        assertEquals(createdUser.getEmail(), newUser.getEmail(), "Email is not the same");
 	}
 
 }
