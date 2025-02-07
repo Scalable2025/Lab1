da2aa48a75c182b6e4a516decbaea5610e27472b
Mohamed Medhat <mmedhat1910@gmail.com>
2025-02-07 21:34:22 +0200

    First Controller

---
+++ src/test/java/com/example/lab1_2/Lab12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
