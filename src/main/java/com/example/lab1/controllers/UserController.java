b1d3e33d440037752685777684e0f923cd297242
Mohamed Medhat <mmedhat1910@gmail.com>
2025-02-07 21:47:30 +0200

    User Model

--- src/main/java/com/example/lab1/controllers/UserController.java
+++ src/main/java/com/example/lab1/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.example.lab1.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
